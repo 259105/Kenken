5f07ef73336bcaa523080bb55c3f1ff8e27454f3
259105 <50930924+259105@users.noreply.github.com>
2020-02-29 18:52:36 +0100

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -11,6 +11,9 @@ Graph GRAPHinit(int V);
 void  GRAPHfree(Graph G);
 Graph GRAPHload(FILE *fin);
 void  GRAPHinsertE(Graph G, int id1, int id2);
+//
+// Created by s259105 on 13/02/2020.
+//
 void  RESOLVEkenken(Graph G);
 
 #endif
